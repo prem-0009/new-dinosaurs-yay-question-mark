8304f25f994d07f3d39f2361ed901dd1d2f32e29
prem <prem.sherpa@codeimmersives.com>
2020-07-24 23:04:33 -0400

    refinement---

--- main.js
+++ main.js
@@ -1,59 +1,40 @@
-const makeDino = function (name, time, eatsMeat, living) {
+const makeDino = function (name, time, eatsMeat, living = false) {
   const dino = {
     species: name,
     period: time,
     carnivore: eatsMeat,
+    extinct: living,
   };
-  if (living === true) {
-    dino.extinct = true;
-  } else if (!living) {
-    dino.extinct = false;
-  }
 
   return dino;
 };
 
-const makeSingular = function (dinosaur) {
-  const withoutUS = {};
-  if (dinosaur.species.slice(-2) === "us") {
-    withoutUS.species = dinosaur.species.slice(0, -2);
-  } 
-  else {
-    withoutUS.species = dinosaur.species;
+const makeSingular = function (dino) {
+  // const withoutUS = {};
+  let newSpecies;
+  if (dino.species.slice(-2) === "us") {
+    newSpecies = dino.species.slice(0, -2);
+  } else {
+    newSpecies = dino.species;
   }
-  withoutUS.period = dinosaur.period;
-  withoutUS.carnivore = dinosaur.carnivore;
-  withoutUS.extinct = dinosaur.extinct;
-
-  return withoutUS;
+return makeDino(newSpecies,dino.period, dino.carnivore, dino.extinct)
+  
 };
 
 const truncateSpecies = function (dino) {
-  const truncatedDino = {};
-  if ( dino.species.length > 10){
-    truncatedDino.species = dino.species.slice(0,7) + '...'
+  let truncatedDino;
+  if (dino.species.length > 10) {
+    truncatedDino = dino.species.slice(0, 7) + "...";
   } else {
-    truncatedDino.species = dino.species;
+    truncatedDino= dino.species;
   }
-
-  truncatedDino.period = dino.period;
-  truncatedDino.carnivore = dino.carnivore;
-  truncatedDino.extinct = dino.extinct;
-
-  return truncatedDino;
+   return makeDino(truncatedDino, dino.period, dino.carnivore, dino.extinct)
+  
 };
 
 const makeExtinct = function (dino) {
-  const extinctDino = {};
-  extinctDino.species = dino.species;
-  extinctDino.extinct=true;
-  extinctDino.period = dino.period;
-  extinctDino.carnivore= dino.carnivore;
-
-  return extinctDino;
-
+  return makeDino(dino.species, dino.period, dino.carnivore, dino.extinct = true)
 };
-
 
 module.exports = {
   makeDino,
@@ -61,6 +42,5 @@ module.exports = {
   truncateSpecies,
   makeExtinct,
 };
-
 
 // console.log(Math)
